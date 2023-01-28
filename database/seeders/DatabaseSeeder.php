3af7c6b54d709d436e3930b29922a04759efe366
feliaazira99 <feliaazira99.com>
2023-01-28 03:00:05 -0800

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'id' => 1,
+                'name'=>'Felia Admin',
+                'email'=>'felia@admin.com',
+                'password'=> Hash::make('wassap99'),
+                'is_admin' => true
+                ]
+        ]);
+    }
+}
